9d7ae6d8e3d5055cffa41d3c3b69d1214d64681c
Stefano <stefano.lamorgia@starcode.it>
2024-08-19 09:58:17 +0200

    first file

--- Graphics101.java
+++ Graphics101.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 public class Graphics101 {    public static void main(String[] args) {
         JFrame frame = new JFrame("Canvas");
         frame.setSize(600, 600);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);          frame.setVisible(true);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);          
+        frame.setVisible(true);  
     }
     
 }
